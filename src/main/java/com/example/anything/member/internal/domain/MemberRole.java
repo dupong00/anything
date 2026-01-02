4c9816b4d5a2e4615791cfc687636ca9a707bf22
Dupong00 <tjdgns9954@gmail.com>
2026-01-02 14:08:04 +0900

    feat: JWT 인증 시스템 구축 및 MEMBER 권한 리팩토링

--- src/main/java/com/example/anything/member/internal/domain/MemberRole.java
+++ src/main/java/com/example/anything/member/internal/domain/MemberRole.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum MemberRole {
-    USER("ROLE_USER"),
+    MEMBER("ROLE_MEMBER"),
     ADMIN("ROLE_ADMIN");
 
     private final String value;
