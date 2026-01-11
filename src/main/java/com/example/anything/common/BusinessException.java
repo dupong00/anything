a51ea31def04817c4ac5109be7600fd125884eb3
Dupong00 <tjdgns9954@gmail.com>
2026-01-11 17:27:43 +0900

    refactor: BusinessException에 serialVersionUID 명시적 선언

--- src/main/java/com/example/anything/common/BusinessException.java
+++ src/main/java/com/example/anything/common/BusinessException.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 
 @Getter
 public class BusinessException extends RuntimeException {
+    //직렬화 시 버전 관리를 위함
+    private static final long serialVersionUID = 1L;
+
     private final ErrorCode errorCode;
 
     public BusinessException(ErrorCode errorCode) {
