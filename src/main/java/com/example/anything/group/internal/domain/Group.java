390b78f921917a0785fa5a354cb74d55eb457d34
Dupong00 <tjdgns9954@gmail.com>
2026-01-15 19:14:25 +0900

    feat: 그룹 모듈 도메인 추가

---
+++ src/main/java/com/example/anything/group/internal/domain/Group.java
@@ -0,0 +1,26 @@
+package com.example.anything.group.internal.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "group")
+public class Group {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+    String invitedCode;
+}
