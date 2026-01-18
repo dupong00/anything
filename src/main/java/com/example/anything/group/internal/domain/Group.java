a7d0b1723511cf1e8ee763af2a3dbec0bb1b5bd9
Dupong00 <tjdgns9954@gmail.com>
2026-01-18 17:29:15 +0900

    feat: Group Domain 및 ErrorCode 정의

--- src/main/java/com/example/anything/group/internal/domain/Group.java
+++ src/main/java/com/example/anything/group/internal/domain/Group.java
@@ -1,10 +1,13 @@
 package com.example.anything.group.internal.domain;
 
+import com.example.anything.common.BusinessException;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,10 +20,44 @@ import lombok.NoArgsConstructor;
 @Builder
 @Table(name = "`group`")
 public class Group {
+    // business
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int CODE_LENGTH = 6;
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    // domain
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
-    String name;
+    private String name;
 
-    String invitedCode;
+    private String invitedCode;
+
+    private LocalDateTime expiredAt;
+
+    private Long ownerId;
+
+    // method
+    public static Group create(String name, Long ownerId) {
+        return Group.builder()
+                .invitedCode(generateInvitedCode())
+                .name(name)
+                .ownerId(ownerId)
+                .expiredAt(LocalDateTime.now().plusDays(1))
+                .build();
+    }
+
+    private static String generateInvitedCode() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            sb.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+
+    public void validateOwner(Long userId) {
+        if (!this.ownerId.equals(userId)) {
+            throw new BusinessException(GroupErrorCode.NOT_GROUP_OWNER);
+        }
+    }
 }
---
+++ src/main/java/com/example/anything/group/internal/domain/GroupErrorCode.java
@@ -0,0 +1,17 @@
+package com.example.anything.group.internal.domain;
+
+import com.example.anything.common.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum GroupErrorCode implements ErrorCode {
+    NOT_GROUP_OWNER(HttpStatus.BAD_REQUEST,"G001", "그룹 관리자가 아닙니다."),
+    GROUP_NOT_FOUND(HttpStatus.BAD_REQUEST,"G002", "그룹이 존재하지 않습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
