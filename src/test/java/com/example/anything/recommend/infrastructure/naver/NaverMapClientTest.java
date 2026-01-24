ce4b0fc550c8f7e02e7d5b84adc6aed29f57df3d
Dupong00 <tjdgns9954@gmail.com>
2026-01-24 22:17:15 +0900

    feat: 환경 변수가 있을때만 테스트 실행 어노테이션 추가

--- src/test/java/com/example/anything/recommend/infrastructure/naver/NaverMapClientTest.java
+++ src/test/java/com/example/anything/recommend/infrastructure/naver/NaverMapClientTest.java
@@ -4,10 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
+@EnabledIfEnvironmentVariable(named = "NAVER_CLOUD_CLIENT_ID", matches = ".+")
+@EnabledIfEnvironmentVariable(named = "NAVER_CLOUD_CLIENT_SECRET", matches = ".+")
 class NaverMapClientTest {
 
     @Autowired
