e6a7a801fcc2867416b0a024a28b5e752fe5584d
Dupong00 <tjdgns9954@gmail.com>
2026-01-25 23:04:00 +0900

    feat: 투표 모듈 어댑터 구현

---
+++ src/main/java/com/example/anything/vote/infrastructure/VoteModuleAdapter.java
@@ -0,0 +1,31 @@
+package com.example.anything.vote.infrastructure;
+
+import com.example.anything.common.BusinessException;
+import com.example.anything.vote.application.port.VoteModulePort;
+import com.example.anything.vote.application.port.WinnerMenuInfo;
+import com.example.anything.vote.internal.domain.BallotBox;
+import com.example.anything.vote.internal.domain.VoteErrorCode;
+import com.example.anything.vote.internal.repository.BallotBoxRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class VoteModuleAdapter implements VoteModulePort {
+    public final BallotBoxRepository ballotBoxRepository;
+    @Override
+    public WinnerMenuInfo getWinnerMenus(Long ballotBoxId) {
+
+        BallotBox ballotBox = ballotBoxRepository.findById(ballotBoxId)
+                .orElseThrow(() -> new BusinessException(VoteErrorCode.BALLOT_BOX_NOT_FOUND));
+
+        List<Long> winnerMenuIds = ballotBox.getWinnerMenuIds();
+
+        return WinnerMenuInfo.builder()
+                .longitude(ballotBox.getLongitude())
+                .latitude(ballotBox.getLatitude())
+                .winnerMenus(winnerMenuIds)
+                .build();
+    }
+}
